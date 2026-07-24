7468816c30417e94273f0dc5c148051fe1856e4b
Glauber Brennon <glauberbrennon@gmail.com>
2026-07-24 18:32:18 -0300

    feat(client): export HttpRequestCounter from client package

--- src/pr_auto_reviewer/infrastructure/client/__init__.py
+++ src/pr_auto_reviewer/infrastructure/client/__init__.py
@@ -1,5 +1,6 @@
 from .git_platform_http_client import GitPlatformHttpClient
+from .http_request_counter import HttpRequestCounter
 from .token_defaults import TokenDefaults
 from .token_resolver import TokenResolver
 
-__all__ = ["GitPlatformHttpClient", "TokenDefaults", "TokenResolver"]
+__all__ = ["GitPlatformHttpClient", "HttpRequestCounter", "TokenDefaults", "TokenResolver"]
