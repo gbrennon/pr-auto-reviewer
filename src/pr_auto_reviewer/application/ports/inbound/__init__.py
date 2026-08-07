1345b697ed98676f74965bd97c8cf0d13c1c8c9b
Glauber Brennon <glauberbrennon@gmail.com>
2026-08-07 05:10:32 -0300

    chore: update __init__.py exports for new modules
    Export new commands, inbound/outbound ports, and application
    services from their respective packages.

--- src/pr_auto_reviewer/application/ports/inbound/__init__.py
+++ src/pr_auto_reviewer/application/ports/inbound/__init__.py
@@ -1,9 +1,19 @@
+from .aggregate_review_findings_use_case import AggregateReviewFindingsUseCase
+from .parse_review_turn_use_case import ParseReviewTurnUseCase
 from .process_issue_commands_use_case import ProcessIssueCommandsUseCase
 from .register_issue_port import RegisterIssuePort
 from .review_pull_request_use_case import ReviewPullRequestUseCase
+from .run_agent_conversation_use_case import RunAgentConversationUseCase
+from .verify_findings_use_case import VerifyFindingsUseCase
+from .run_multi_phase_review_use_case import RunMultiPhaseReviewUseCase
 
 __all__ = [
+    "AggregateReviewFindingsUseCase",
+    "ParseReviewTurnUseCase",
     "ProcessIssueCommandsUseCase",
     "RegisterIssuePort",
     "ReviewPullRequestUseCase",
+    "VerifyFindingsUseCase",
+    "RunAgentConversationUseCase",
+    "RunMultiPhaseReviewUseCase",
 ]
--- src/pr_auto_reviewer/application/services/__init__.py
+++ src/pr_auto_reviewer/application/services/__init__.py
@@ -1,9 +1,19 @@
-from .review_pull_request_service import ReviewPullRequestService
+from .agent_conversation_service import AgentConversationService
+from .finding_aggregator import FindingAggregator
+from .finding_verifier import FindingVerifier
+from .multi_phase_review_orchestrator import MultiPhaseReviewOrchestrator
 from .process_issue_commands_service import ProcessIssueCommandsService
 from .register_issue_service import RegisterIssueService
+from .review_pull_request_service import ReviewPullRequestService
+from .turn_parser import TurnParser
 
 __all__ = [
-    "ReviewPullRequestService",
+    "AgentConversationService",
+    "FindingAggregator",
+    "FindingVerifier",
+    "MultiPhaseReviewOrchestrator",
     "ProcessIssueCommandsService",
     "RegisterIssueService",
+    "ReviewPullRequestService",
+    "TurnParser",
 ]
